d86bd1e002db50dc7f69ee0b64ea439adac82ced
Md Khussairiee Bin Ahmad <111945064+mdkhussairiee@users.noreply.github.com>
2023-04-10 21:59:39 +0800

    Update dialogflowgpt3.js

--- dialogflowgpt3.js
+++ dialogflowgpt3.js
@@ -37,10 +37,10 @@ app.post("/dialogflow", express.json(), (req, res) => {
     
       const completionParmas = {
         prompt: dialog.join('\n'),
-        max_tokens: 60,
-        temperature: 0.85,
+        max_tokens: 250,
+        temperature: 0.6,
         n: 1,
-        stream: false,
+        stream: true,
         logprobs: null,
         echo: false,
         stop: '\n',
